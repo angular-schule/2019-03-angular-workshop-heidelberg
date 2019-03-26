2223fc97989159d8a76be9bbfcdc012c8a49a906
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-26 12:12:58 +0100

    TODO: neue Klasse zum Addieren von Werten zB. math.ts und dann: Math.sum(2, 3)

--- typescript-playground/index.ts
+++ typescript-playground/index.ts
@@ -3,3 +3,7 @@ import { Test } from "./test";
 let test = new Test(`Hallo Heidelberg! ❤️`);
 var antwort = test.dieAntwort('Die Antwort auf...');
 console.log(antwort)
+
+
+// TODO: neue Klasse zum Addieren von Werten
+// zB. math.ts und dann: Math.sum(2, 3)
---
+++ typescript-playground/webpack.config.js
@@ -0,0 +1,15 @@
+// webpack.config.js
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./index.ts",
+    output: { filename: "bundle.js" },
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: "ts-loader" }
+      ]
+    }
+  };
