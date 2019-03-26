7a065d5cf88da0341fb175ba78687cd0496ebe23
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-26 12:29:53 +0100

    hello class Math

--- typescript-playground/index.ts
+++ typescript-playground/index.ts
@@ -1,4 +1,5 @@
 import { Test } from "./test";
+import { Math } from "./math";
 
 let test = new Test(`Hallo Heidelberg! ❤️`);
 var antwort = test.dieAntwort('Die Antwort auf...');
@@ -7,3 +8,5 @@ console.log(antwort)
 
 // TODO: neue Klasse zum Addieren von Werten
 // zB. math.ts und dann: Math.sum(2, 3)
+
+console.log(Math.sum(2, 3));
---
+++ typescript-playground/math.ts
@@ -0,0 +1,9 @@
+// TODO: neue Klasse zum Addieren von Werten
+// zB. math.ts und dann: Math.sum(2, 3)
+
+
+export class Math {
+    static sum(number1: number, number2: number) {
+        return number1 + number2;
+    }
+}
