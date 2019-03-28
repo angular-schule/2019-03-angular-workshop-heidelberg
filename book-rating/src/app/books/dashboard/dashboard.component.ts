93b22a3258b0991b12421bb68f81463d41c7e8db
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-28 15:42:48 +0100

    "Hausaufgabe"

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -13,29 +14,22 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor(private service: BookRatingService) {
+  constructor(private service: BookRatingService, private store: BookStoreService) {
+
+    /*
+    "Hausaufgabe":
+    Erweitere das Buch-Interface im ein Thumbnail
+    1. informiere dich über die Verfügbaren Properties mithilfe von Swagger UI
+    2. erweitere das Interface Book
+    3. zeige das Thumbnail anstelle der Katzen an!
+    */
+
   }
 
   ngOnInit() {
-
-    this.books = [{
-      isbn: '111',
-      title: 'Angular',
-      description: 'Tolles Buch',
-      rating: 5
-    },
-    {
-      isbn: '222',
-      title: 'AngularJs',
-      description: 'End of Lifetime erreicht',
-      rating: 3
-    },
-    {
-      isbn: '333',
-      title: 'React',
-      description: '...',
-      rating: 1
-    }];
+    this.store.getAll().subscribe((books) => {
+      this.books = books;
+    });
   }
 
   doRateDown(book: Book) {
