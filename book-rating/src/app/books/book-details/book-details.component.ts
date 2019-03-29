222ebe17b80ffc930eb0f0b31614469b3fee7f85
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-29 09:50:46 +0100

    rxjs: observer, oberservable, subscription

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BookStoreService } from '../shared/book-store.service';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable, of, from } from 'rxjs';
+import { Book } from '../shared/book';
+
 
 @Component({
   selector: 'br-book-details',
@@ -9,17 +13,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookDetailsComponent implements OnInit {
 
-  isbn: string;
+  isbn$: Observable<string>;
+  book$: Observable<Book>;
 
   constructor(private store: BookStoreService,
               private route: ActivatedRoute) {}
 
   ngOnInit() {
-    // this.isbn = this.route.snapshot.paramMap.get('isbn');
+    this.isbn$ = this.route.paramMap
+      .pipe(
+        map(params => params.get('isbn'))
+      );
 
-    this.route.paramMap.subscribe((params) => {
-      this.isbn = params.get('isbn');
-    });
+    // --------
+    // import { map } from 'rxjs/operators';
+    // import { Observable, of, from } from 'rxjs';
+
+    const observer = {
+      next: zahl => console.log(zahl),
+      error: e => console.log('ERROR', e),
+      complete: () => console.log('Complete! 😀')
+    };
+
+    const observable = of(1, 2, 3);
+
+    const subscription = observable.subscribe(observer);
+    subscription.unsubscribe();
   }
 
 }
