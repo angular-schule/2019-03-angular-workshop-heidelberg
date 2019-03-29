44e940f7b392b627a1cbf9d7fd8ea1af56a61689
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-29 11:59:19 +0100

    details

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookStoreService } from '../shared/book-store.service';
 import { ActivatedRoute } from '@angular/router';
-import { map, filter } from 'rxjs/operators';
-import { Observable, of, from } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Book } from '../shared/book';
 
 
@@ -13,22 +13,16 @@ import { Book } from '../shared/book';
 })
 export class BookDetailsComponent implements OnInit {
 
-  isbn$: Observable<string>;
   book$: Observable<Book>;
 
   constructor(private store: BookStoreService,
               private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.paramMap
+    this.book$ = this.route.paramMap
       .pipe(
         map(params => params.get('isbn')),
-        map(isbn => this.store.getSingle(isbn))
-      ).subscribe(books$ => {
-
-        books$.subscribe((book) => {
-          console.log(book);
-        });
-      });
+        switchMap(isbn => this.store.getSingle(isbn))
+      );
   }
 }
